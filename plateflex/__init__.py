323db1ede94abd42e89e0ceeccec0b00a8bad48f
Pascal Audet <pascal.audet@uottawa.ca>
2019-12-15 09:44:57 -1000

    PEP8 __init__

--- plateflex/__init__.py
+++ plateflex/__init__.py
@@ -9,8 +9,8 @@
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -118,20 +118,29 @@ Once the previous steps are performed, you can install ``plateflex`` using pip:
 
 
 """
+
+__version__ = '0.1.0'
+
+__author__ = 'Pascal Audet'
+
+
 # -*- coding: utf-8 -*-
 from . import conf as cf
 from . import estimate
 from . import plotting
-from .classes import Grid, TopoGrid, GravGrid, BougGrid, FairGrid, RhocGrid, ZcGrid, Project
+from .classes import Grid, TopoGrid, GravGrid, BougGrid, FairGrid
+from .classes import RhocGrid, ZcGrid, Project
 from .cpwt import conf_cpwt as cf_wt
 from .flex import conf_flex as cf_fl
+
 
 def set_conf_cpwt():
     cf_wt.k0 = 5.336
 
+
 def set_conf_flex():
     cf_fl.zc = 35.*1.e3
-    cf_fl.rhom = 3200. 
+    cf_fl.rhom = 3200.
     cf_fl.rhoc = 2700.
     cf_fl.rhow = 1030.
     cf_fl.rhoa = 0.
@@ -139,8 +148,10 @@ def set_conf_flex():
     cf_fl.wd = 0.
     cf_fl.boug = 1
 
+
 set_conf_cpwt()
 set_conf_flex()
+
 
 def get_conf_cpwt():
     """
@@ -159,7 +170,9 @@ def get_conf_cpwt():
     print('\n'.join((
         'Wavelet parameter used in plateflex.cpwt:',
         '-----------------------------------------',
-        '[Internal wavenumber]      k0 (float):     {0:.3f}'.format(cf_wt.k0))))
+        '[Internal wavenumber]      k0 (float):     {0:.3f}'.format(
+            cf_wt.k0))))
+
 
 def get_conf_flex():
     """
@@ -184,12 +197,19 @@ def get_conf_flex():
     print('\n'.join((
         'Global model parameters currently in use by plateflex:',
         '------------------------------------------------------',
-        '[Crustal thickness]        zc (float):     {0:.0f} m'.format(cf_fl.zc), 
-        '[Mantle density]           rhom (float):   {0:.0f} kg/m^3'.format(cf_fl.rhom),
-        '[Crustal density]          rhoc (float):   {0:.0f} kg/m^3'.format(cf_fl.rhoc),
-        '[Water density]            rhow (float):   {0:.0f} kg/m^3'.format(cf_fl.rhow),
-        '[Air density]              rhoa (float):   {0:.0f} kg/m^3'.format(cf_fl.rhoa),
-        '[Fluid density]            rhof (float):   {0:.0f} kg/m^3'.format(cf_fl.rhof),
-        '[Water depth]              wd (float):     {0:.0f} m'.format(cf_fl.wd),
-        '[Bouguer analysis?]        boug (int):     {0} ; {1}'.format(cf_fl.boug, bool(cf_fl.boug)))))
-
+        '[Crustal thickness]        zc (float):     {0:.0f} m'.format(
+            cf_fl.zc),
+        '[Mantle density]           rhom (float):   {0:.0f} kg/m^3'.format(
+            cf_fl.rhom),
+        '[Crustal density]          rhoc (float):   {0:.0f} kg/m^3'.format(
+            cf_fl.rhoc),
+        '[Water density]            rhow (float):   {0:.0f} kg/m^3'.format(
+            cf_fl.rhow),
+        '[Air density]              rhoa (float):   {0:.0f} kg/m^3'.format(
+            cf_fl.rhoa),
+        '[Fluid density]            rhof (float):   {0:.0f} kg/m^3'.format(
+            cf_fl.rhof),
+        '[Water depth]              wd (float):     {0:.0f} m'.format(
+            cf_fl.wd),
+        '[Bouguer analysis?]        boug (int):     {0} ; {1}'.format(
+            cf_fl.boug, bool(cf_fl.boug)))))
