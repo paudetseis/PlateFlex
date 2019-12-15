14e69e04a02bec9d9df363d186d8b3c041004fd9
Pascal Audet <pascal.audet@uottawa.ca>
2019-12-15 09:38:34 -1000

    PEP8 doc

--- plateflex/doc.py
+++ plateflex/doc.py
@@ -9,8 +9,8 @@
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -45,7 +45,6 @@ def install_doc(path="./PlateFlex-Examples"):
 
     Notebooks_Path = _pkg_resources.resource_filename(
         "plateflex", "examples")
-    
 
     ct = _dir_util.copy_tree(
         Notebooks_Path,
